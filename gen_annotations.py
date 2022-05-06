8795823b59deb32194d474dc1255fb55775b5a47
branvu <brangogohi@gmail.com>
2022-05-06 04:02:05 -0700

    update to gen_annotations

--- gen_annotations.py
+++ gen_annotations.py
@@ -7,27 +7,27 @@ file_obj = open("annotations.csv", "a")
 # american - 0, japanese - 1, irish - 2
 # prefix for category is language
 
-# for im_name in os.listdir(PATH3):
-#     # asl-0-1
-#     category = "0" + im_name[4]
-#     annotation = PATH + "/" + im_name + ",0," + category + "\n"
-#     file_obj.write(annotation)
-#     print("writing ", annotation)
+for im_name in os.listdir(PATH3):
+    # asl-0-1
+    liststr = im_name.split('-')
+    category = "0" + liststr[1]
+    annotation = PATH + "/" + im_name + ",0," + category + "\n"
+    file_obj.write(annotation)
+    print("writing ", annotation)
 
-# # Read from irish
-# for im_name in os.listdir(PATH2):
-#     # isl-0
-#     category = "2" + im_name[4]
-#     annotation = PATH + "/" + im_name + ",2," + category + "\n"
-#     file_obj.write(annotation)
-#     print("writing ", annotation)
+# Read from irish
+for im_name in os.listdir(PATH2):
+    # isl-0
+    liststr = im_name.split('-')
+    category = "2" + liststr[1]
+    annotation = PATH + "/" + im_name + ",2," + category + "\n"
+    file_obj.write(annotation)
+    print("writing ", annotation)
 
 # # Read from folder JSL
 for im_name in os.listdir(PATH):
-    i = len(im_name) - 5
-    while i > 0 and im_name[i].isnumeric():
-        i -= 1
-    category = "1" + im_name[i + 1:len(im_name) - 4]
+    liststr = im_name.split('-')
+    category = "1" + liststr[1]
     annotation = PATH + "/" + im_name + ",1," + category + "\n"
     file_obj.write(annotation)
     print("writing ", annotation)
